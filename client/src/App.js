dd13283a63fbb6462155b3b63d47e676929aeace
Tasneem <t.haidar.ny@gmail.com>
2026-03-30 00:26:51 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
+  const [signupEmail, setSignupEmail] = useState("");
+  const [signupPassword, setSignupPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>FocusForm</h1>
